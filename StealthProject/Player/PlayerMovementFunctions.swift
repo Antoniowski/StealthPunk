87c92603df10f715f1dc00c1f8a753b02ebe847d
Antonio Romano <r97.antonio@gmail.com>
2022-05-03 17:19:38 +0200

    Cambiato schema file

---
+++ StealthProject/Player/PlayerMovementFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerMovementFunctions.swift
+//  StealthProject
+//
+//  Created by Antonio Romano on 03/05/22.
+//
+
+import Foundation
+import SpriteKit
+
+func playerMovement(player: SKShapeNode, velocity: CGVector){
+    player.position.x += velocity.dx
+    player.position.y += velocity.dy
+}
