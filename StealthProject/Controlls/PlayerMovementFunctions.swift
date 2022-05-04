4e53f83db994641cd900587836b3d1ae407bdfb5
Antonio Romano <r97.antonio@gmail.com>
2022-05-04 15:48:06 +0200

    iniziati cambiamenti movimento

--- StealthProject/Controlls/PlayerMovementFunctions.swift
+++ StealthProject/Controlls/PlayerMovementFunctions.swift
@@ -7,10 +7,34 @@
 
 import Foundation
 import SpriteKit
+import SwiftUI
 
 func playerMovement(player: SKShapeNode, velocity: CGVector){
     player.position.x += velocity.dx
     player.position.y += velocity.dy
+}
+
+
+protocol PlayableScene{
+    var inputVector: CGVector {get set}
+    var rollVector: CGVector {get set}
+    
+    func moveState()
+    func attackState()
+    func rollState()
+    func interactState()
+}
+
+extension PlayableScene{
+    mutating func moveState(){
+        self.inputVector = CGVector.zero
+        inputVector = myMovement
+        if inputVector != CGVector.zero{
+            rollVector = inputVector
+            inputVector = inputVector
+        }
+        
+    }
 }
 
 
