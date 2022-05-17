10e5a5fe893eb6af775c03769cff03f97c554fe2
Antonio Romano <r97.antonio@gmail.com>
2022-05-17 14:12:25 +0200

    aggiunti collezionabili

--- StealthProject/Controlls/PlayerMovementFunctions.swift
+++ StealthProject/Controlls/PlayerMovementFunctions.swift
@@ -47,9 +47,9 @@ extension PlayableScene{
         inputVector = myMovement
         if inputVector != CGVector.zero{
             rollVector = inputVector.normalized()
-            inputVector = inputVector*ACCELLERATION*delta //AGGIUNGERE ACCELERAZIONE APPROPRIATA
+            inputVector = inputVector*ACCELLERATION*Double(player.getSpeed())*0.25*delta //AGGIUNGERE ACCELERAZIONE APPROPRIATA
             velocity += inputVector
-            velocity = velocity.clamped(maxLength: MAX_SPEED*delta*0.5)
+            velocity = velocity.clamped(maxLength: MAX_SPEED*delta*Double(player.getSpeed())*0.5)
         }else{
             velocity = velocity.moveTowardZero(value: FRICTION*delta)
         }
@@ -61,9 +61,9 @@ extension PlayableScene{
         inputVector = myMovement
         if inputVector != CGVector.zero{
             rollVector = inputVector.normalized()
-            inputVector = inputVector*ACCELLERATION*delta //AGGIUNGERE ACCELERAZIONE APPROPRIATA
+            inputVector = inputVector*ACCELLERATION*Double(player.getSpeed())*0.25*delta //AGGIUNGERE ACCELERAZIONE APPROPRIATA
             velocity += inputVector
-            velocity = velocity.clamped(maxLength: 1.5*MAX_SPEED*delta)
+            velocity = velocity.clamped(maxLength: MAX_SPEED*Double(player.getSpeed())*delta)
         }else{
             velocity = velocity.moveTowardZero(value: FRICTION*delta)
         }
