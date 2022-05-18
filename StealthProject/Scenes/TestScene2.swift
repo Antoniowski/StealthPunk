3f9195563741086a9f2d6c651a404a75158bf008
Martina Ottaviano <martina99ottaviano@gmail.com>
2022-05-18 15:57:41 +0200

    Ora ci si può nascondere nell'armadio yay

--- StealthProject/Scenes/TestScene2.swift
+++ StealthProject/Scenes/TestScene2.swift
@@ -128,7 +128,7 @@ class TestScene2: SKScene, PlayableScene, SKPhysicsContactDelegate {
         
         
         addChild(player)
-//        addChild(armadio)
+        addChild(armadio)
 //        addChild(lampione)
 //        addChild(luce)
 //        addChild(t)
