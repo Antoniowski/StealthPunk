ae50cd04742b98768d528adb6940e6e0cefb4e73
GwenDL98 <guenda.dl98@gmail.com>
2022-05-26 17:02:32 +0200

    aggiunto animazione cespuglio aperto

--- StealthProject/Objects/Hideouts/BushClass.swift
+++ StealthProject/Objects/Hideouts/BushClass.swift
@@ -14,7 +14,7 @@ class Bush: Hideout{
         super.init(texture: SKTexture(imageNamed: "cespuglio"), highlighted: SKTexture(imageNamed: "cespuglioBlur"), color: .clear, size: CGSize(width: blocco, height: blocco), type: .HIDEOUT)
         self.setHideoutCategory(.BUSH)
         self.setName("Bush")
-        self.openBush = SKTexture(imageNamed: "cespuglio")
+        self.openBush = SKTexture(imageNamed: "cespuglioAperto")
         self.normalTexture = SKTexture(imageNamed: "cespuglio normal map")
         self.physicsBody = SKPhysicsBody(rectangleOf: .init(width: Double(blocco)/1.5, height: Double(blocco)/2.5), center: .init(x: 0, y: Double(blocco)/4))
         self.physicsBody?.isDynamic = false
