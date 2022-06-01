c616c0997579d5a2a4ecc9e45807d958de640cc3
GwenDL98 <guenda.dl98@gmail.com>
2022-06-01 14:25:50 +0200

    audio

--- StealthProject/AudioHandler.swift
+++ StealthProject/AudioHandler.swift
@@ -7,3 +7,28 @@
 
 import Foundation
 import AVFoundation
+
+
+class Music{
+    
+    static let instance = Music()
+    
+    let doorTaverna : String = "doorTaverna"
+    let door : String = "door"
+    let door2 : String = "door2"
+    let panca : String = "pancaOpenCorto"
+    let bushExit : String = "BushExit"
+    let bushEnter : String = "BushEnter"
+    let cabinetOpen : String = "closeCabinet"
+    let cabinetClose : String = "openCabinet"
+    let vetrna : String = "vetrina"
+    let bonk : String = "BONK"
+    let gear : String = "GearSound"
+    let pagina : String = "pagina"
+    let footstep1 : String = "SingleFootstep"
+    let footstep2 : String = "SingleFootstep2"
+    let swing : String = "swing"
+    let footstep3 : String = "footsteps"
+    let grassfootstep : String = "grassFootsteps"
+    
+}
